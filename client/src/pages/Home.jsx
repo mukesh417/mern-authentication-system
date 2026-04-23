1e13a3540feec4c7c1841b5b1e097521f5c8b59e
Mukesh Jaiswal <jaiswalmukesh267@gmail.com>
2026-04-23 15:35:12 +0530

    Fix: Final UI background path

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Header from '../components/Header'
-import { assets } from '../assets/assets'
 
 const Home = () => {
   return (
-    <div 
-      className='flex flex-col items-center justify-center min-h-screen bg-cover bg-center'
-      style={{ backgroundImage: `url(${assets.bg_img})` }}
-    >
+    <div className='flex flex-col items-center justify-center min-h-screen bg-[url("/bg_img.png")] bg-cover bg-center'>
         <Navbar/>
         <Header/>
     </div>
