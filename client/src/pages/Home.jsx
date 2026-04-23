089fac79655f29fc34f07b41e42cadc4e49b3204
Mukesh Jaiswal <jaiswalmukesh267@gmail.com>
2026-04-23 15:27:08 +0530

    Fix: Final background UI fix

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Header from '../components/Header'
+import { assets } from '../assets/assets'
 
 const Home = () => {
   return (
-    <div className='flex flex-col items-center justify-center min-h-screen bg-[url("bg_img.png")] bg-cover bg-center'>
+    <div 
+      className='flex flex-col items-center justify-center min-h-screen bg-cover bg-center'
+      style={{ backgroundImage: `url(${assets.bg_img})` }}
+    >
         <Navbar/>
         <Header/>
     </div>
